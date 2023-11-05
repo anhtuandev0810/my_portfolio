bbe232a9c294b9a5dc7d767028dd8f3c5cbe4ae8
anhtuandev0810 <anhtuandev081@gmail.com>
2023-11-06 00:28:49 +0700

    Add code

---
+++ app.js
@@ -0,0 +1,7 @@
+let textTyped = new Typed(".text", {
+    strings: ["Frontend Developer", "Backend Developer", "Mobile Developer"],
+    typeSpeed: 90,
+    backSpeed: 90,
+    backDelay: 900,
+    loop: true
+})
