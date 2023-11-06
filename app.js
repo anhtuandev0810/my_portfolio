24d7b5a505851095e9d558095b8aca75407f8bcc
anhtuandev0810 <anhtuandev081@gmail.com>
2023-11-06 23:10:21 +0700

    Update navbar and responsive

--- app.js
+++ app.js
@@ -4,8 +4,35 @@ let textTyped = new Typed(".text", {
     backSpeed: 90,
     backDelay: 900,
     loop: true
-})
+});
+let menu = document.querySelector('.menu');
+let menuIcon = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+let menuCancel = document.querySelector(".menu-cancel");
 
+
+console.log(navbar.children);
+const arrList = Array.prototype.slice.call( navbar.children );
+arrList.forEach(element => {
+    element.addEventListener('click', () => {
+    navbar.style.display = 'none';
+    })
+});
+
+let isTrue = true;
+
+function handleToggle(res) {
+    res.children[0].style.display = 'none';
+    navbar.style.display = 'flex';
+    menuCancel.style.display = 'block';
+}
+
+function handleCancel(response) {
+    response.children[0].style.display = 'none';
+    navbar.style.display = 'none';
+    menuIcon.style.display = 'block';
+}
+ 
 function myFunc() {
     window.location.href = './uni.html';
 }
