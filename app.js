bd4d274941f24fe800eeb04b92f39220ee38cb4d
anhtuandev0810 <anhtuandev081@gmail.com>
2023-11-06 16:51:54 +0700

    update education

--- app.js
+++ app.js
@@ -5,3 +5,28 @@ let textTyped = new Typed(".text", {
     backDelay: 900,
     loop: true
 })
+
+function myFunc() {
+    window.location.href = './uni.html';
+}
+
+function myReactFunc() {
+    window.location.href = './react-edu.html';
+}
+
+
+function myHtmlFunc() {
+    window.location.href = './html-edu.html';
+}
+
+function myNodeFunc() {
+    window.location.href = './node-edu.html';
+}
+
+function myDataFunc() {
+    window.location.href = './data-edu.html';
+}
+
+function myAppFunc() {
+    window.location.href = './app-edu.html';
+}
